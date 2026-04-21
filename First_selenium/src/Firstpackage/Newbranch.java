abaeb3ecb16fa3191a0f0e9b4a6eed0fc60bf4c6
sravani <sravani@LAPTOP-M0PBE2QE>
2026-04-21 10:44:05 +0530

    committing into the new branch.

---
+++ First_selenium/src/Firstpackage/Newbranch.java
@@ -0,0 +1,14 @@
+package Firstpackage;
+
+public class Newbranch {
+
+	public static void main(String[] args) {
+		System.out.println("Hello from new branch!!");
+		int a=10;
+		int b=5;
+		int c=a+b;
+		System.out.println("The addition of two numbers:" +c);
+
+	}
+
+}
